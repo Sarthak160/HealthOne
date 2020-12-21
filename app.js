49819eb263b514ae6c3d1fc9128319e334b25b45
Sarthak160 <50234097+Sarthak160@users.noreply.github.com>
2020-12-21 18:17:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 const flash = require('connect-flash');
 const session = require('express-session');
 
+// hi everyone
 const app = express();
 
 // Passport Config
